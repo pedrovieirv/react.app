262783252127b5543877bb084247e14dc0ef3a55
pedrovieirv <pv417868@gmail.com>
2023-09-06 13:39:43 -0300

    ahh

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>óla mundo</h1>
+      <p>meu prinmeiro priojeto</p>
+    </div>
+  );
+}
+
+export default App;
